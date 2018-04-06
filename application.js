5e2f4bb2f0a0e27932884ebb2bdfdd6ecbbc2d44
swenwx <511854671@qq.com>
2018-04-06 20:19:36 +0800

    feat(all): 增加koa，并修改部分文档

--- application.js
+++ application.js
@@ -142,6 +142,10 @@ let EventEmitter = require('events');
 
 
 // next 实现中间件模式
+// 在use middleware 的时候，将所有fn收集，
+// next使用async和await 将状态传递
+// 当一个中间件调用 next() 则该函数暂停并将控制传递给定义的下一个中间件。
+// 当在下游没有更多的中间件执行后，堆栈将展开并且每个中间件恢复执行其上游行为。
 
 class Application extends EventEmitter{
   constructor(){
--- request.js
+++ request.js
@@ -1,4 +1,12 @@
-let url = require('url');
+const URL = require('url').URL;
+const net = require('net');
+const contentType = require('content-type');
+const stringify = require('url').format;
+const parse = require('parseurl')
+const qs = require('querystring');
+const typeis = require('type-is');
+const fresh = require('fresh');
+const only = require('only');
 
 // url.parse(urlString,boolean,boolean) 返回一个url解析对象，
 // 第二个参数为true时，query属性返回对象，否则字符串
@@ -6,5 +14,44 @@ let url = require('url');
 module.exports = {
   get query() {
     return url.parse(this.req.url, true).query;
+  },
+
+  // header
+  get header() {
+    return this.req.headers;
+  },
+
+  set header(val) {
+    this.req.headers = val;
+  },
+
+  // headers关联header
+  get headers() {
+    return this.req.headers;
+  },
+
+  set headers(val) {
+    this.req.headers = val;
+  },
+
+
+
+  // url
+  get url() {
+    return this.req.url;
+  },
+
+  set url(val){
+    this.req.url = val;
+  },
+
+  // orgin
+  get origin(){
+    return `${this.protocol}://${this.host}`
+  },
+
+  // href
+  get href(){
+
   }
 }
